00adb0b25ffc77dbaf919cfec2ce7a40ebeacdfa
RusinovaKN <denisova.90@mail.ru>
2023-06-23 19:08:05 +0300

    remove console

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,19 @@
 function checksString(string, length) {
   return string.length <= length;
 }
-console.log(checksString('00005', 5));
+checksString(767676, 5);
 
 function checksPalindrom(string) {
   const normalString = string.replaceAll(' ', '').toLowerCase();
-  console.log(normalString);
-
   let reverseString = '';
   for (let i = normalString.length - 1; i >= 0; i--) {
     reverseString += normalString.at(i);
   }
-  console.log(reverseString);
   return normalString === reverseString;
 }
-console.log(checksPalindrom('Лёша на полке клопа нашёл'));
-console.log(checksPalindrom('топот'));
-console.log(checksPalindrom('ДовОд'));
+checksPalindrom('Лёша на полке клопа нашёл');
+checksPalindrom('топот');
+checksPalindrom('ДовОд');
 
 function extractsNumber(string) {
   string = string.toString();
@@ -30,12 +27,11 @@ function extractsNumber(string) {
   return parseInt(symbols, 10);
 }
 
-console.log('');
-console.log(extractsNumber('2023 год'));
-console.log(extractsNumber('ECMAScript 2022'));
-console.log(extractsNumber('1 кефир, 0.5 батона'));
-console.log(extractsNumber('агент 007'));
-console.log(extractsNumber('а я томат'));
-console.log(extractsNumber(2023));
-console.log(extractsNumber(-1));
-console.log(extractsNumber(1.5));
+extractsNumber('2023 год');
+extractsNumber('ECMAScript 2022');
+extractsNumber('1 кефир, 0.5 батона');
+extractsNumber('агент 007');
+extractsNumber('а я томат');
+extractsNumber(2023);
+extractsNumber(-1);
+extractsNumber(1.5);
